4da35f432a1c486b4668ebcb0fc1fa46c337adb1
petroniojs <petroniojs@yahoo.com.br>
2025-05-03 08:50:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ fig_criticidade = px.bar(
     color='criticidade',
     title='Quantidade de Produtos por Criticidade em Cada Distrito',
     barmode='group',
-    text='quantidade'
+    text='quantidade',
     color_discrete_map={
         "Crítico": "#FF4C4C",    # vermelho
         "Alerta": "#FFA500",     # laranja
